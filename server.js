f51ad9111aafbc701368a281340f923a925af3d4
Vyom <chvyom1234@gmail.com>
2025-12-19 19:56:37 +0530

    local host removed

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ const menuRoutes=require('./routes/menuRoutes')
 app.use('/menu',menuRoutes)
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+  console.log('Server is running')
 })
